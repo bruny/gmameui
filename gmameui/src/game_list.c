5fb9c433c09ad87189798ed521fba45466bbebe2
bruny <adb@iinet.net.au>
2008-02-18 13:29:20 +0000

    Removed debugging code

--- gmameui/src/game_list.c
+++ gmameui/src/game_list.c
@@ -463,7 +463,6 @@ gamelist_load (void)
 						    "%s", tmp_array[ (j * 2) + offset]);
 					rom->cpu_info[j].sound_flag = FALSE;
 				}
-GMAMEUI_DEBUG ("CPU at position %d is %s", j, rom->cpu_info[j].name);
 				rom->cpu_info[j].clock = atoi (tmp_array[ (j * 2) + offset + 1]);
 			}
 
@@ -476,7 +475,6 @@ GMAMEUI_DEBUG ("CPU at position %d is %s", j, rom->cpu_info[j].name);
 					g_snprintf (rom->sound_info[j].name, MAX_CPU,
 						    "%s", tmp_array[offset + (j * 2)]);
 				}
-GMAMEUI_DEBUG ("Sound CPU at position %d is %s", j, rom->sound_info[j].name);
 				rom->sound_info[j].clock = atoi (tmp_array[offset + (j * 2) + 1]);
 			}
 
